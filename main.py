dd0e65a9209f2d6fdc7ab415701dd602094a4c94
陈刑 <chenxing.io@outlook.com>
2024-10-16 23:40:47 +0800

    get_id

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from ansi import color_cat
+from utils import input_to, write
 
 
 welcome = "welcome"
 
 
-color_cat(welcome)
+class Player:
+    def __init__(self):
+        self.id = ""
+
+    def __str__(self):
+        return f"Player {{ id: {self.id} }}"
+
+
+def logon():
+    color_cat(welcome)
+    player = Player()
+    write("您的英文名字(新玩家可以选择一喜欢的名字)：")
+    input_to(get_id, player)
+
+
+def get_id(arg: str, player):
+    player.id = arg
+    print(player)
+
+
+if __name__ == "__main__":
+    logon()
--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
 def read_file(file_path: str):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
+
+
+def input_to(fun: callable, *args):
+    fun(input(), *args)
+
+
+def write(value: str):
+    print(value, end="")
